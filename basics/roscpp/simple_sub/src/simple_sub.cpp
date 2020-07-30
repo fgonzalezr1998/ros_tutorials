31af1215eed390632624d3361bb589191a82d33a
fgonzalezr1998 <fergonzaramos@yahoo.es>
2020-07-31 00:55:48 +0200

    [basics]Basic packages for learn ROS modified

---
+++ basics/roscpp/simple_sub/src/simple_sub.cpp
@@ -0,0 +1,21 @@
+#include <ros/ros.h>
+#include <std_msgs/Int32.h>
+
+void
+callback(const std_msgs::Int32::ConstPtr msg)
+{
+	ROS_INFO("I received: %d\n", msg->data);
+}
+
+int
+main(int argc, char **argv)
+{
+	ros::init(argc, argv, "simple_sub_node");
+
+	ros::NodeHandle nh;
+	ros::Subscriber sub = nh.subscribe("/talk", 1, callback);
+
+	ros::spin();
+
+exit(EXIT_SUCCESS);
+}
---
+++ basics/roscpp/simple_sub/src/simple_sub_iterative.cpp
@@ -0,0 +1,57 @@
+#include <ros/ros.h>
+#include <std_msgs/Int32.h>
+
+class Subscriber
+{
+public:
+
+	Subscriber()
+	:	received_(false)
+	{
+		ROS_INFO("%s\n", "I'm the Subscriber");
+		sub_ = nh_.subscribe("/talk", 1, &Subscriber::callback, this);
+	}
+
+	void
+	doWork()
+	{
+		if(!received_)
+			return;
+
+		ROS_INFO("I received: %d\n", recv_msg_.data);
+		received_ = false;
+	}
+
+private:
+
+	void
+	callback(const std_msgs::Int32::ConstPtr msg)
+	{
+		recv_msg_ = *msg;
+		received_ = true;
+	}
+
+	ros::NodeHandle nh_;
+	ros::Subscriber sub_;
+	std_msgs::Int32 recv_msg_;
+	bool received_;
+};
+
+int
+main(int argc, char **argv)
+{
+	ros::init(argc, argv, "simple_sub_iterative_node");
+
+	Subscriber sub;
+
+	//¿Que pasa si le ponemos 1Hz?
+
+	ros::Rate loop_rate(1);
+	while(ros::ok()){
+		sub.doWork();
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+
+	exit(EXIT_SUCCESS);
+}

8cf5ba87b7bed3741530d51f7f629d1d18addf93
fgonzalezr1998 <fergonzaramos@yahoo.es>
2020-08-02 22:34:27 +0200

    [basics]rospy publishers finished

---
+++ basics/rospy/simple_pub_py/src/simple_pub_class.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+class Publisher():
+    def __init__(self):
+        self.pub = rospy.Publisher("/talk", String, queue_size=1)
+
+    def doWork(self):
+        msg = String()
+        msg.data = "Hello World"
+        self.pub.publish(msg)
+
+def main():
+    rospy.init_node("simple_pub_node")
+
+    publisher = Publisher()
+
+    # 2Hz Rate:
+
+    rate = rospy.Rate(2)
+    while(not rospy.is_shutdown()):
+        publisher.doWork()
+        rate.sleep()
+
+if __name__ == "__main__":
+
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
---
+++ basics/rospy/simple_pub_py/src/simple_pub_iterative_class.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int32
+
+class Publisher():
+    def __init__(self):
+        self.pub = rospy.Publisher("/talk", Int32, queue_size=1)
+        self.msg = Int32()
+        self.msg.data = 0
+
+    def doWork(self):
+        self.pub.publish(self.msg)
+        self.msg.data += 1
+
+def main():
+    rospy.init_node("simple_pub_node")
+
+    publisher = Publisher()
+
+    # 2Hz Rate:
+
+    rate = rospy.Rate(2)
+    while(not rospy.is_shutdown()):
+        publisher.doWork()
+        rate.sleep()
+
+if __name__ == "__main__":
+
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
